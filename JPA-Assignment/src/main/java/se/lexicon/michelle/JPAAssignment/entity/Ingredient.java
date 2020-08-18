5cbb70f45557ecf095eff832cc5fe472f6c2ca80
michelle <michelle.johansson85@gmail.com>
2020-08-18 08:23:55 +0200

    Ingredient.java * added ingredient class

---
+++ JPA-Assignment/src/main/java/se/lexicon/michelle/JPAAssignment/entity/Ingredient.java
@@ -0,0 +1,48 @@
+package se.lexicon.michelle.JPAAssignment.entity;
+
+import java.util.Objects;
+
+public class Ingredient {
+    private int ingredientId;
+    private String ingredientName;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+
+    public int getIngredientId() {
+        return ingredientId;
+    }
+
+    public String getIngredientName() {
+        return ingredientName;
+    }
+
+    public void setIngredientName(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return ingredientId == that.ingredientId &&
+                Objects.equals(ingredientName, that.ingredientName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ingredientId, ingredientName);
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" + "ingredientId=" + ingredientId +
+                ", ingredientName='" + ingredientName + '\'' +
+                '}';
+    }
+}
