7937dc5f873e3fafe936818b59e3c83f3fd1756d
michelle <michelle.johansson85@gmail.com>
2020-08-18 08:20:03 +0200

    init commit

---
+++ JPA-Assignment/src/main/java/se/lexicon/michelle/JPAAssignment/JpaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.michelle.JPAAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAssignmentApplication.class, args);
+	}
+
+}
---
+++ JPA-Assignment/src/test/java/se/lexicon/michelle/JPAAssignment/JpaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.michelle.JPAAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
