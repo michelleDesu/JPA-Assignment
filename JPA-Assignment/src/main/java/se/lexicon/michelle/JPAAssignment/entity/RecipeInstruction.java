ad5cc1af6c7385581575a05c69cd94ab7ee2da3c
michelle <michelle.johansson85@gmail.com>
2020-08-18 08:52:44 +0200

    RecipeInstruction.java * added RecipeInstruction class

---
+++ JPA-Assignment/src/main/java/se/lexicon/michelle/JPAAssignment/entity/RecipeInstruction.java
@@ -0,0 +1,48 @@
+package se.lexicon.michelle.JPAAssignment.entity;
+
+import java.util.Objects;
+
+public class RecipeInstruction {
+    private String instructionId;
+    private String instructions;
+
+    public RecipeInstruction() {
+    }
+
+    public RecipeInstruction(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getInstructionId() {
+        return instructionId;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RecipeInstruction that = (RecipeInstruction) o;
+        return Objects.equals(instructionId, that.instructionId) &&
+                Objects.equals(instructions, that.instructions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(instructionId, instructions);
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeInstruction{" + "instructionId='" + instructionId + '\'' +
+                ", instructions='" + instructions + '\'' +
+                '}';
+    }
+}
