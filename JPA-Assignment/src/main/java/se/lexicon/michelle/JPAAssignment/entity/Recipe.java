33bc77203f4994a0e1ba4d9a7193ef756c968db7
michelle <michelle.johansson85@gmail.com>
2020-08-18 08:49:39 +0200

    Recipe.java * added Recipe class

---
+++ JPA-Assignment/src/main/java/se/lexicon/michelle/JPAAssignment/entity/Recipe.java
@@ -0,0 +1,85 @@
+package se.lexicon.michelle.JPAAssignment.entity;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Recipe {
+    private int recipeId;
+    private String recipeName;
+    private List<RecipeIngredient> recipeIngredients;
+    private RecipeInstruction instruction;
+    private List<RecipeCategory> categories;
+
+    public Recipe() {
+    }
+
+    public Recipe(String recipeName, List<RecipeIngredient> recipeIngredients, RecipeInstruction instruction, List<RecipeCategory> categories) {
+        this.recipeName = recipeName;
+        this.recipeIngredients = recipeIngredients;
+        this.instruction = instruction;
+        this.categories = categories;
+    }
+
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public List<RecipeIngredient> getRecipeIngredients() {
+        return recipeIngredients;
+    }
+
+    public void setRecipeIngredients(List<RecipeIngredient> recipeIngredients) {
+        this.recipeIngredients = recipeIngredients;
+    }
+
+    public RecipeInstruction getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(RecipeInstruction instruction) {
+        this.instruction = instruction;
+    }
+
+    public List<RecipeCategory> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<RecipeCategory> categories) {
+        this.categories = categories;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Recipe recipe = (Recipe) o;
+        return recipeId == recipe.recipeId &&
+                Objects.equals(recipeName, recipe.recipeName) &&
+                Objects.equals(recipeIngredients, recipe.recipeIngredients) &&
+                Objects.equals(instruction, recipe.instruction) &&
+                Objects.equals(categories, recipe.categories);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(recipeId, recipeName, recipeIngredients, instruction, categories);
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" + "recipeId=" + recipeId +
+                ", recipeName='" + recipeName + '\'' +
+                ", recipeIngredients=" + recipeIngredients +
+                ", instruction=" + instruction +
+                ", categories=" + categories +
+                '}';
+    }
+}
